ece29d84dc38f6fc23de02a0174100a1c0a46195
g-mothy <gobillamothy85@gmail.com>
2019-02-10 01:43:49 +0530

    int

--- script.py
+++ script.py
@@ -1,13 +1,18 @@
 import os
 
 root="complete directory of the folder"
-
+count = 0
 def clear_files(root,extension):
     for path, subdirs, files in os.walk(root):
         for name in files:
             if name.endswith('.'+ extension):
                 p=os.path.join(path, name)
                 os.remove(p)
+                global count
+                count = count+1
                 print(name)
                 
-clear_files(root,'vtt')
+root = input("Enter the dir: ")
+extension = input("Enter the extension of the files to remove: ")
+clear_files(root,extension)
+print(str(count)+" files are deleted")
